ff295bb990775a8a9df4fe3e1339a2f2c3dc2b4e
Brian Broll <brian.broll@gmail.com>
2020-04-21 09:28:01 -0500

    Add more RPCs in WordEmbeddings service

--- netsblox-services/services/hello_python/__init__.py
+++ netsblox-services/services/hello_python/__init__.py
@@ -2,16 +2,16 @@
 This is a simple example of a NetsBlox service in Python.
 """
 
-import netsblox
+import netsblox as nb
 from netsblox import types
 
-@netsblox.rpc(help='I am the simplest type of RPC')
+@nb.rpc('I am the simplest type of RPC')
 def hello():
     return 'world'
 
-@netsblox.rpc(help='I can add numbers!')
-@netsblox.argument('x', type=types.Number, help='The first number to sum')
-@netsblox.argument('y', type=types.Number, help='The second number to sum')
+@nb.rpc('I can add numbers!')
+@nb.argument('x', type=types.Number, help='The first number to sum')
+@nb.argument('y', type=types.Number, help='The second number to sum')
 def sum_nums(x, y):
     return x + y
 
--- netsblox-services/services/word_embeddings/__init__.py
+++ netsblox-services/services/word_embeddings/__init__.py
@@ -1,24 +1,65 @@
 """
-Train custom word embeddings from a corpus
+Train custom word embeddings using gensim.
+
+For more information about gensim, check out https://radimrehurek.com/gensim/
 """
 
-import netsblox
+import netsblox as nb
 from netsblox import types
-#from gensim.models import Word2Vec
+from gensim.models import Word2Vec
+from gensim.test.utils import common_texts
+import os
+from os import path
 
-# @netsblox.rpc(help='Train a word2vec model and save it')
-# @netsblox.argument('text', type=types.String, help='The first number to sum')
-# @netsblox.argument('name', type=types.String, help='The second number to sum')
-# def trainWord2Vec(text, save_name):
-    # return 'world'
+models_dir = path.join(path.dirname(__file__), 'models')
+os.makedirs(models_dir, exist_ok=True)
 
-@netsblox.rpc(help='List available trained models')
+@nb.rpc(help='Train a word2vec model and save it')
+@nb.argument('wordLists', type=types.List, help='List of word lists')
+@nb.argument('saveName', type=types.String, help='Name for trained model')
+@nb.argument('size', type=types.Int, help='Dimensionality of the word vectors', optional=True)
+@nb.argument('window', type=types.Int, help='Max distance between current and predicted word', optional=True)
+@nb.argument('minCount', type=types.Int, help='Ignore words with fewer than this number of occurrences', optional=True)
+def trainWord2Vec(wordLists, saveName, size=100, window=5, minCount=2):
+    model = Word2Vec(wordLists, size=size, window=window, min_count=minCount)
+    model.save(path.join(models_dir, saveName))
+    return 'Model saved as ' + saveName
+
+@nb.rpc('Get the number of words in the vocabulary')
+@nb.argument('modelName', type=types.String, help='Name of trained model')
+def getWord2VecVocabSize(modelName):
+    model = Word2Vec.load(path.join(models_dir, modelName))
+    return model.wv.vectors.shape[0]
+
+@nb.rpc('Get the entire vocabulary of a trained model.\n\nWarning: this can be quite large.')
+@nb.argument('modelName', type=types.String, help='Name of trained model')
+def getWord2VecVocab(modelName):
+    model = Word2Vec.load(path.join(models_dir, modelName))
+    return list(model.wv.vocab.keys())
+
+@nb.rpc('Get the top-N most similar words. Positive words contribute positively to similarity; negative words contribute negatively')
+@nb.argument('modelName', type=types.String, help='Name of trained model')
+@nb.argument('positive', type=types.List, help='Words that contribute positively')
+@nb.argument('negative', type=types.List, help='Words that contribute negatively', optional=True)
+@nb.argument('count', type=types.Int, help='Number of words to return', optional=True)
+def getMostSimilar(modelName, positive, negative=[], count=5):
+    model = Word2Vec.load(path.join(models_dir, modelName))
+    return model.wv.most_similar(positive, negative, count)
+
+@nb.rpc('Get the vector representation for a given word')
+@nb.argument('modelName', type=types.String, help='Name of trained model to use')
+@nb.argument('word', type=types.String)
+def getWordVector(modelName, word):
+    model = Word2Vec.load(path.join(models_dir, modelName))
+    vector = model.wv.word_vec(word)
+    return [ float(n) for n in vector ]
+
+
+@nb.rpc(help='Fetch example text (common texts from gensim)')
+def exampleText():
+    return common_texts
+
+@nb.rpc(help='List available trained models')
 def listModels():
-    return []
-
-# @netsblox.rpc(help='I can add numbers!')
-# @netsblox.argument('x', type=types.String, help='The first number to sum')
-# @netsblox.argument('y', type=types.String, help='The second number to sum')
-# def sum_nums(x, y):
-    # return x + y
+    return os.listdir(models_dir)
 
