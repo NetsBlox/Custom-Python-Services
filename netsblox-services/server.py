4ce1a0f3b7e1064074058533d41615077424ee03
Brian Broll <brian.broll@gmail.com>
2020-04-21 09:27:40 -0500

    Better serialization of RPC response

--- netsblox-services/server.py
+++ netsblox-services/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask import request
+import json
+
 app = Flask(__name__)
 CORS(app)
 
@@ -17,15 +19,11 @@ def favicon():
 
 @app.route('/<service>')
 def service_metadata(service):
-    print('service', service)
     service = services.get(service)
     return jsonify(service.metadata)
 
 @app.route('/<service>/<rpc>', methods=['POST'])
 def invoke_rpc(service, rpc):
     service = services.get(service)
-    # TODO: Get the client ID, project ID, role ID
-    # TODO: Validate the request?
-    # TODO: get arguments
     arg_data = request.json
-    return str(service.invoke_rpc(rpc, arg_data))
+    return json.dumps(service.invoke_rpc(rpc, arg_data))
