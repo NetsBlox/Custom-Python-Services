f22f3786cf24182b7b87025fcd137f03330497c5
Brian Broll <brian.broll@gmail.com>
2020-04-29 17:51:31 -0500

    Rename WordEmbeddings -> Word2Vec

--- netsblox-services/services/word2vec/__init__.py
+++ netsblox-services/services/word2vec/__init__.py
@@ -1,5 +1,5 @@
 """
-Train custom word embeddings using gensim.
+Train custom word2vec models using gensim.
 
 For more information about gensim, check out https://radimrehurek.com/gensim/
 """
@@ -11,29 +11,30 @@ from gensim.test.utils import common_texts
 import os
 from os import path
 
+service_name = 'Word2Vec'
 models_dir = path.join(path.dirname(__file__), 'models')
 os.makedirs(models_dir, exist_ok=True)
 
 @nb.rpc('Train a word2vec model and save it')
-@nb.argument('wordLists', type=types.List, help='List of word lists')
+@nb.argument('sentences', type=types.List, help='List of word lists')
 @nb.argument('saveName', type=types.String, help='Name for trained model')
 @nb.argument('size', type=types.Integer, help='Dimensionality of the word vectors', optional=True)
 @nb.argument('window', type=types.Integer, help='Max distance between current and predicted word', optional=True)
 @nb.argument('minCount', type=types.Integer, help='Ignore words with fewer than this number of occurrences', optional=True)
-def trainWord2Vec(wordLists, saveName, size=100, window=5, minCount=2):
-    model = Word2Vec(wordLists, size=size, window=window, min_count=minCount)
+def trainModel(sentences, saveName, size=100, window=5, minCount=2):
+    model = Word2Vec(sentences, size=size, window=window, min_count=minCount)
     model.save(path.join(models_dir, saveName))
     return 'Model saved as ' + saveName
 
 @nb.rpc('Get the number of words in the vocabulary')
 @nb.argument('modelName', type=types.String, help='Name of trained model')
-def getWord2VecVocabSize(modelName):
+def getVocabSize(modelName):
     model = Word2Vec.load(path.join(models_dir, modelName))
     return model.wv.vectors.shape[0]
 
 @nb.rpc('Get the entire vocabulary of a trained model.\n\nWarning: this can be quite large.')
 @nb.argument('modelName', type=types.String, help='Name of trained model')
-def getWord2VecVocab(modelName):
+def getVocab(modelName):
     model = Word2Vec.load(path.join(models_dir, modelName))
     return list(model.wv.vocab.keys())
 
@@ -42,7 +43,7 @@ def getWord2VecVocab(modelName):
 @nb.argument('positive', type=types.List, help='Words that contribute positively')
 @nb.argument('negative', type=types.List, help='Words that contribute negatively', optional=True)
 @nb.argument('count', type=types.Integer, help='Number of words to return', optional=True)
-def getMostSimilar(modelName, positive, negative=[], count=5):
+def getMostSimilarWords(modelName, positive, negative=[], count=5):
     model = Word2Vec.load(path.join(models_dir, modelName))
     return model.wv.most_similar(positive, negative, count)
 
@@ -57,15 +58,23 @@ def getWordVector(modelName, word):
 
 @nb.rpc('Fetch example text (common texts from gensim)')
 def exampleText():
-    return common_texts
+    first_sentence = ['these', 'are', 'example', 'sentences']
+    sentences = [first_sentence];
+    sentences.extend(common_texts)
+    return sentences
 
 @nb.rpc('List available trained models')
 def listModels():
     return os.listdir(models_dir)
 
+# @nb.rpc('List public trained models')
+# def listPublicModels():
+    # return os.listdir(models_dir)
+
 @nb.rpc('Delete trained model')
 @nb.argument('modelName', type=types.String, help='Name of trained model to delete')
 def deleteModel(modelName):
+    # TODO: check permissions
     os.remove(path.join(models_dir, modelName))
     return os.listdir(models_dir)
 
